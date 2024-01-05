6bbd985c9d43275a8e3a51a8298b7c1a33b4c845
Marina Pires <marinapiressg@gmail.com>
2024-01-05 18:37:07 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Smartphone Nokia");
+        Nokia nokia = new Nokia(numero: "992589658", modelo: "Nokia01", imei: "8585962145", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Instagram");
+        nokia.ReceberLigacao();
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone");
+        Iphone iphone = new Iphone(numero: "985485486", modelo: "Iphone 15", imei: "8844863254", memoria: 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Whatsapp");
+        iphone.ReceberLigacao();
+    }
+}
+
